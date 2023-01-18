d11916953cfac4bf124eaa2c5a342e1a8ff75f65
Junho Park <dwarfthema@gmail.com>
2023-01-18 22:28:02 +0900

    update:main:mouseFollower

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,34 @@
 import { Inter } from "@next/font/google";
 import Layout from "../components/layout";
 import Link from "next/link";
+import { useRef } from "react";
+import UseFollowPointer from "../components/useFollowPointer";
+import { motion } from "framer-motion";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const mouseFollower = useRef(null);
+  const { x, y } = UseFollowPointer(mouseFollower);
   return (
     <Layout seoTitle="Home" isMain={true}>
       <div className="bg-black text-white flex justify-center ">
         <div className="h-screen flex flex-col items-center justify-center w-64">
+          <motion.div
+            ref={mouseFollower}
+            className=" h-10 w-10 rounded-full border-white border-dotted border-2 absolute"
+            animate={{ x, y }}
+            transition={{
+              type: "spring",
+              damping: 20,
+            }}
+          />
           <div className="text-4xl font-medium text-center">
             VivleThema
             <br />
             3D Interactive
           </div>
-          <ul className="mt-7 items-start w-full list-disc">
+          <ul className="mt-7 items-start w-full list-disc z-10">
             <li className="">
               <Link href="/works/bounce_material_ball" className="text-xl">
                 Bounce Material Ball
